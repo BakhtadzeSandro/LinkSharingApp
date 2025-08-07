f14b7f8b9ffdd2b17ccb987ef7c54ab62ef1155e
BakhtadzeSandro <sandrobakhtadze1909@gmail.com>
2025-08-07 23:27:03 +0400

    build profile details view

--- apps/frontend/src/app/pages/main/components/profile-details/profile-details.ts
+++ apps/frontend/src/app/pages/main/components/profile-details/profile-details.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-profile-details',
-  imports: [],
+  imports: [TranslateModule, FormsModule, ReactiveFormsModule, InputTextModule],
   templateUrl: './profile-details.html',
-  styleUrl: './profile-details.scss'
+  styleUrl: './profile-details.scss',
+  standalone: true,
 })
 export class ProfileDetails {
-
+  constructor() {}
 }
