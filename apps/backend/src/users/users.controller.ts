337e6773a1333a93d88a1eb9515b3a9a3a7ef96c
BakhtadzeSandro <sandrobakhtadze1909@gmail.com>
2025-10-07 22:10:54 +0400

    feat: add current user route

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Get('me')
+  getCurrentUser(@Req() req: Request) {
+    return this.usersService.getCurrentUser(req['user'].userId);
+  }
+}
--- apps/backend/src/users/users.service.ts
+++ apps/backend/src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './users.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private usersModel: Model<User>,
+  ) {}
+  async getCurrentUser(userId: string) {
+    return await this.usersModel.findById(userId).select('-password');
+  }
+}
